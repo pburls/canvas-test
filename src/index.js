b416b16eb8fbec5891dc5a155a885f252fc8f381
Patrick Burls <pburls@gmail.com>
2019-02-06 22:56:12 +0000

    add konva canvas

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Stage, Layer, Rect, Text } from 'react-konva';
 
-const Index = () => {
-    return <div>Hello React!</div>;
-};
+const Index = () => (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+            <Text text="Try click on rect" />
+        </Layer>
+    </Stage>
+);
 
 ReactDOM.render(<Index />, document.getElementById("root"));
