a4943e7c36d318346db4d0c87051f60d580ebbf7
Patrick Burls <pburls@gmail.com>
2019-02-06 23:07:53 +0000

    first two rects with arrow

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import { Stage, Layer, Rect, Text, Arrow } from 'react-konva';
 
 const Index = () => (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
+    <Stage width={400} height={200}>
         <Layer>
             <Text text="Try click on rect" />
+            <Rect
+                x={20}
+                y={20}
+                width={50}
+                height={50}
+                fill={'green'}
+            />
+            <Rect
+                x={120}
+                y={20}
+                width={50}
+                height={50}
+                fill={'green'}
+            />
+            <Arrow x={70} y={45} points={[0, 0, 50, 0]} stroke={'black'} strokeWidth={4} />
         </Layer>
     </Stage>
 );
